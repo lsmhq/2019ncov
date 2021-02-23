e126191caaeb5596f95f05221c3b4d8331fa105b
daifang <784433957@qq.com>
2021-02-23 14:01:04 +0800

    webView

---
+++ webView/App.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import {
+  BackHandler,
+  ToastAndroid
+} from 'react-native';
+import AutoHeightWebView from 'react-native-autoheight-webview'
+
+import React, { Component } from 'react'
+let flag = false
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      canGoBack:false
+    }
+  }
+  componentDidMount(){
+    let that = this
+    BackHandler.addEventListener('hardwareBackPress',()=>{
+      console.log(flag)
+      if(that.state.canGoBack){
+        that.refs['webView'].goBack();
+        return true;
+      }else{
+        if(flag){
+          // that.props.navigator.pop();
+          return false;
+        }else{
+          ToastAndroid.show('再按一次退出应用',ToastAndroid.SHORT);
+          flag = true
+          setTimeout(()=>{
+            flag = false
+          },2000)
+          return true;
+        }
+      }
+    })
+  }
+  onNavigationStateChange=(nav)=>{
+    // console.log(nav)
+    this.setState({
+      canGoBack:nav.canGoBack
+    })
+  }
+  render() {
+    return (
+      <AutoHeightWebView
+      // originWhitelist={['*']}
+      ref="webView"
+      style={{flex:1}}
+      javaScriptEnabled={true}
+      source={{uri: 'http://10.4.72.207:3000'}}
+      scalesPageToFit={false}
+      onNavigationStateChange={this.onNavigationStateChange}
+      
+    />
+    )
+  }
+}
